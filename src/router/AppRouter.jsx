26046ee8348409a2231a983bf0972046a13afe06
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-19 14:55:43 -0400

    archivos barril

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { LoginPage } from '../auth/pages/';
-import { MarvelPage, DcPage } from '../heroes/pages/';
+import { LoginPage } from '../auth/pages';
+import { MarvelPage, DcPage } from '../heroes';
 import { Navbar } from '../ui/components';
 
 export const AppRouter = () => {
