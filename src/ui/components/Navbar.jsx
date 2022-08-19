d36147ae0052933eae5a8b3341ef930211fddc05
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-19 17:11:52 -0400

    navigate push / replace - useNavigate

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    // Toma del Navigation.Provider info de las rutas
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true // evita que pueda ingresar al historial anterior
+        });
+    }
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
@@ -46,6 +55,7 @@ export const Navbar = () => {
 
                   <button
                     className="nav-item nav-link btn"
+                    onClick={ onLogout }
                     >
                     Logout
                   </button>
