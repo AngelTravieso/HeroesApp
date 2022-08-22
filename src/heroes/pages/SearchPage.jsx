e17d6506a206a86917e3ac787c3b15a870487267
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-22 15:42:29 -0400

    mostrar mensajes condicionales

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -17,7 +17,10 @@ export const SearchPage = () => {
   // Obtener heroe por el nombre (busqueda input)
   const heroes = getHeroesByName(q);
 
-  const { searchText, onInputChange, onResetForm } = useForm({
+  const showSearch = ( q.length === 0 );
+  const showError = ( q.length > 0 ) && heroes.length === 0;
+
+  const { searchText, onInputChange } = useForm({
     searchText: q,
   });
 
@@ -25,7 +28,7 @@ export const SearchPage = () => {
   const onSearchSubmit = (e) => {
     e.preventDefault();
 
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
 
     // Navega a la misma pagina
     navigate(`?q=${ searchText.toLowerCase().trim() }`);
@@ -64,13 +67,26 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">
+          {/* {
+            ( q === '' )
+            ? (<div className="alert alert-primary">Search a hero</div>)
+            : ( heroes.length === 0 )
+              && (<div className="alert alert-danger">No hero with <b>{ q }</b> </div>)
+          } */}
+
+          <div className="alert alert-primary animate__animated animate__fadeIn" 
+            style={{ display: showSearch ? '' : 'none' }}
+          >
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}
+            >
             No hero with <b>{ q }</b>
           </div>
+
 
           {
             heroes.map( hero => (
