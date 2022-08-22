52dca570e563dea71b24518ea0fb5459b57f0966
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-22 17:52:50 -0400

    mantener el usuario activo

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,24 +3,37 @@ import { AuthContext } from './AuthContext';
 import { authReducer } from './AuthReducer';
 import { types } from '../types/types';
 
-const initialState = {
-    logged: false,
+
+// const initialState = {
+//     logged: false,
+// }
+
+
+const init = () => {
+  const user = JSON.parse( localStorage.getItem('user') );
+
+  return {
+    logged: !!user,
+    user,
+  }
 }
+
 
 export const AuthProvider = ({ children }) => {
 
-  const [ authState, dispatch ] = useReducer( authReducer, initialState );
+  const [ authState, dispatch ] = useReducer( authReducer, {}, init );
 
 
   const login = ( name = '' ) => {
 
+    const user = { id: 'ABC', name }
+
     const action = {
       type: types.login,
-      payload: {
-        id: 'ABC',
-        name: name,
-      }
+      payload: user
     }
+
+    localStorage.setItem('user', JSON.stringify( user ) );
 
     dispatch( action );
 
