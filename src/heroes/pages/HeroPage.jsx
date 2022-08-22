2e1760749d330ebe2beb2598ec6660faf72994ce
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-22 14:17:56 -0400

    estilo del componente hero screen

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { useNavigate, Navigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
@@ -7,13 +7,41 @@ export const HeroPage = () => {
 
   const hero = getHeroById( id );
 
+  const navigate = useNavigate();
+
+  const onNavigateBack = () => {
+    // Regresa a la pantalla anterior (puede sacar al usuario de la pagina)
+    navigate( -1 );
+  }
+
   if ( !hero ) {
     return <Navigate to="/marvel" />
   }
   
   return (
-    <>
-      <h1>{ hero.superhero }</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={ `/assets/heroes/${ id }.jpg` } alt={ hero.superhero } className="img-thumbnail" />
+      </div>
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego:</b> {hero.alter_ego }</li>
+          <li className="list-group-item"><b>Publisher:</b> {hero.publisher }</li>
+          <li className="list-group-item"><b>First Appearence:</b> {hero.first_appearance}</li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+          <p>{ hero.characters }</p>
+
+        <button
+          className="btn btn-outline-primary"
+          onClick={ onNavigateBack }
+          >
+          Regresar
+        </button>
+      </div>
+
+    </div>
   )
 }
