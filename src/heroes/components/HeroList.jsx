85ba5089d778e7e3f5c7ee37b5cdc7644439e7a9
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-22 14:21:31 -0400

    agregar useMemo

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,10 +1,11 @@
+import { useMemo } from "react";
 import { HeroCard } from "./";
 import { getHeroesByPublisher } from "../helpers"
 
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher( publisher );
+    const heroes = useMemo( () => getHeroesByPublisher( publisher ), [ publisher ] );
 
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { useNavigate, Navigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers';
 
@@ -5,7 +6,7 @@ export const HeroPage = () => {
 
   const { id } = useParams();
 
-  const hero = getHeroById( id );
+  const hero = useMemo( () => getHeroById( id ), [ id ]);
 
   const navigate = useNavigate();
 
