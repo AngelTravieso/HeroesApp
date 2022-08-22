1ecda9a8b0ab9d420150a9c77f07d1acd80186f5
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-22 14:06:51 -0400

    leer argumentos por URL

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,7 +1,19 @@
-
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
+
+  const { id } = useParams();
+
+  const hero = getHeroById( id );
+
+  if ( !hero ) {
+    return <Navigate to="/marvel" />
+  }
+  
   return (
-    <h1>HeroPage</h1>
+    <>
+      <h1>{ hero.superhero }</h1>
+    </>
   )
 }
